da6571f989e041706fca6b14367c60114a8c848b
Roshan <roshan2613@gmail.com>
2020-05-01 21:47:37 -0500

    Update App.js
    Did Assign-6 using import()

--- src/App.js
+++ src/App.js
@@ -1,12 +1,93 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Nav from './Nav';
-import Sport from './Sport';
-import Food from './Food';
-import Monument from './Monument';
-import Nature from './Nature';
-import Car from './Car';
 import {BrowserRouter as Router,Switch, Route } from 'react-router-dom';
+
+
+//import Nav from './Nav';
+// import Sport from './Sport';
+// import Food from './Food';
+// import Monument from './Monument';
+// import Nature from './Nature';
+// import Car from './Car';
+
+
+
+class DynamicImport extends Component {
+  state = {
+    component: null
+  }
+  componentDidMount () {
+    this.props.load()
+      .then((component) => {
+        this.setState(() => ({
+          component: component.default ? component.default : component
+        }))
+      })
+  }
+  render() {
+    return this.props.children(this.state.component)
+  }
+}
+
+//Nav bar
+const Nav = (props) => (
+  <DynamicImport load={() => import('./Nav')}>
+    {(Component) => Component === null
+      ? <p>Loading</p>
+      : <Component {...props} />}
+  </DynamicImport>
+)
+
+//Sport
+const Sport = (props) => (
+  <DynamicImport load={() => import('./Sport')}>
+    {(Component) => Component === null
+      ? <p>Loading</p>
+      : <Component {...props} />}
+  </DynamicImport>
+)
+
+
+//Food
+const Food = (props) => (
+  <DynamicImport load={() => import('./Food')}>
+    {(Component) => Component === null
+      ? <p>Loading</p>
+      : <Component {...props} />}
+  </DynamicImport>
+)
+
+
+//Monument ( which i am not using in my react app) 
+const Monument = (props) => (
+  <DynamicImport load={() => import('./Monument')}>
+    {(Component) => Component === null
+      ? <p>Loading</p>
+      : <Component {...props} />}
+  </DynamicImport>
+)
+
+
+//Nature 
+const Nature = (props) => (
+  <DynamicImport load={() => import('./Nature')}>
+    {(Component) => Component === null
+      ? <p>Loading</p>
+      : <Component {...props} />}
+  </DynamicImport>
+)
+
+
+//Car
+const Car = (props) => (
+  <DynamicImport load={() => import('./Car')}>
+    {(Component) => Component === null
+      ? <p>Loading</p>
+      : <Component {...props} />}
+  </DynamicImport>
+)
+
+
 
 function App() {
   return (
